2b43384a052d1228dfc3e7d28c8eef95dfb04e75
Abhishek Katnoria <abhishekkatnoria993@gmail.com>
2024-02-10 18:16:40 +0530

    add some more functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
 import Input from "./Component/Input.js";
-import TaskDone from "./Component/TaskDone.js";
 
 function App() {
   return <>
   <Input />
-  
   </>;
 }
 
--- src/Component/Input.js
+++ src/Component/Input.js
@@ -1,16 +1,14 @@
 import React, { useState } from "react";
-import "./App1.css";
-import TodoList from "./TodoList";
-import TaskDone from "./TaskDone";
+import "./Input.css";
+import cut1 from "./img/delete.png";
 
 function Input() {
   const [task, SetValue] = useState("");
   const [message, Setmessage] = useState([]);
-
+  const [complete, setComlete] = useState([]);
   const handleClick = (event) => {
     SetValue(event.target.value);
   };
-  
 
   const handleBtn = () => {
     if (!task) {
@@ -19,48 +17,79 @@ function Input() {
       SetValue("");
     }
   };
-
   const handlebtn2 = (id) => {
     const newitem = message.filter((items, idx) => {
       return idx !== id;
     });
     Setmessage(newitem);
   };
-  return (
-    
-      <div className="main-div">
-        <div className="center-div">
-          <br />
-          <h1>ToDo List</h1>
-          <br />
-          <input
-            type="text"
-            placeholder="Add a Items"
-            onChange={handleClick}
-            name="input"
-            value={task}
-            className="todo-input"
-          />
-          <button className="btn-input" onClick={handleBtn}>
-            +
-          </button>
+  const handleBox = (id) => {
+    const completeTask = message[id];
+    const update = message.filter((elem, ind) => {
+      return id !== ind;
+    });
+    Setmessage(update);
+    setComlete([...complete, completeTask]);
+  };
 
-          <ol>
-            {message.map((elem, ind) => {
-              return (
-                <TodoList
-                  key={ind}
-                  id={ind}
-                  text={elem}
-                  onSelect={handlebtn2}
-                />
-              );
-            })}
-          </ol>
-        </div>
-        <TaskDone/>
+  return (
+    <div className="main-div">
+      <div className="center-div">
+        <br />
+        <h1>ToDo List</h1>
+        <br />
+        <input
+          type="text"
+          placeholder="Add a Items"
+          onChange={handleClick}
+          name="input"
+          value={task}
+          className="todo-input"
+        />
+        <button className="btn-input" onClick={handleBtn}>
+          +
+        </button>
+        <ol className="list-contianer-first">
+          {message.map((elem, ind) => {
+            return (
+              <div className="todo-style" key={ind}>
+                <div className="box-style">
+                  <input
+                    type="checkbox"
+                    className="cheak-box"
+                    onChange={() => {
+                      handleBox(ind);
+                    }}
+                  />
+                  <li className="task-list-first" l>
+                    {elem}
+                  </li>
+                </div>
+                <div className="img-style">
+                  <img
+                    alt="#"
+                    src={cut1}
+                    onClick={() => {
+                      handlebtn2(ind);
+                    }}
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </ol>
       </div>
-   
+      <div className="com-center-div">
+        <br />
+        <h1 className="list-com">Completed Task</h1>
+        <ol className="list-container-sec">
+          <br />
+          {complete.map((elemt, indt) => {
+            return <li className="task-list-sec">{elemt}</li>;
+          })}
+        </ol>
+      </div>
+    </div>
   );
 }
 
